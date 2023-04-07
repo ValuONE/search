95f647d12278e4ebb0c483a17a095c58ef99f6c8
ValuONE <vapiendl@gmail.com>
2023-04-07 02:10:51 +0200

    [+] Created config.rs

---
+++ src/model/config.rs
@@ -0,0 +1,22 @@
+pub struct Config {
+    pub search_type: SearchType,
+    pub query: String,
+    pub dir: Option<String>,
+    pub case_sensitive: bool
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            search_type: SearchType::LocateFile,
+            query: "".to_string(),
+            dir: None,
+            case_sensitive: false,
+        }
+    }
+}
+
+pub enum SearchType {
+    LocateFile,
+    FindString
+}
