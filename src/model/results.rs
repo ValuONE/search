28b4791a110746d80cd5083d811638ee8963b26d
ValuONE <vapiendl@gmail.com>
2023-04-07 23:27:35 +0200

    [/] Edited results.rs

--- src/model/results.rs
+++ src/model/results.rs
@@ -1,21 +1,40 @@
-#[derive(Debug)]
+use crate::model::config::SearchType;
+use crate::model::config::SearchType::{FindString, LocateFile};
+
 pub struct FinalResult {
     pub file_count: i64,
+    pub search_type: SearchType,
     pub results: Vec<SearchResult>
+}
+
+impl FinalResult {
+    pub fn display(self) {
+        if self.search_type == LocateFile {
+            for (i, result) in self.results.into_iter().enumerate() {
+                println!("      [{}] > {}\n\n", i + 1, result.filename);
+            }
+
+            return;
+        }
+
+        for (i, result) in self.results.into_iter().enumerate() {
+            println!("      [{}] > {} in line(s): {:?}", i + 1, result.filename, result.line);
+        }
+    }
 }
 
 impl Default for FinalResult {
     fn default() -> Self {
         Self {
+            search_type: FindString,
             file_count: 0,
             results: vec![]
         }
     }
 }
 
-#[derive(Debug)]
 pub struct SearchResult {
-    pub filename: Vec<String>,
+    pub filename: String,
     pub content: Vec<String>,
     pub line: Vec<i64>
 }
@@ -23,7 +42,7 @@ pub struct SearchResult {
 impl Default for SearchResult {
     fn default() -> Self {
         Self {
-            filename: vec![],
+            filename: "".to_string(),
             content: vec![],
             line: vec![]
         }
