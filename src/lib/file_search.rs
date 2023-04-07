41bfb275434bed0a0c2d1991ca49ec0510b27927
ValuONE <vapiendl@gmail.com>
2023-04-07 23:27:20 +0200

    [/] Edited file_search.rs

--- src/lib/file_search.rs
+++ src/lib/file_search.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use jwalk::{Parallelism};
+use crate::lib::cli_feedback::setup_progress_indication;
 
 use crate::lib::content_search;
 use crate::model::config::SearchType;
@@ -8,6 +9,7 @@ use crate::model::results::{FinalResult, SearchResult};
 
 pub fn search(dirs: Vec<String>, query: &str, search_type: SearchType, sensitive: bool) -> FinalResult {
     let mut result = FinalResult::default();
+    result.search_type = search_type.clone();
 
     for dir in dirs {
         jwalk::WalkDir::new(dir)
@@ -19,12 +21,15 @@ pub fn search(dirs: Vec<String>, query: &str, search_type: SearchType, sensitive
                 if dir_entry.file_type().is_file() {
                     result.file_count += 1;
 
-                    if !sensitive && search_type == LocateFile && dir_entry.file_name() == query ||
-                        sensitive && search_type == LocateFile && dir_entry.file_name().to_ascii_lowercase().as_os_str() == query.to_lowercase().as_str() {
+                    let dir_name = dir_entry.file_name();
+                    if !sensitive {
+                        query.to_lowercase().as_str();
+                        let dir_name = dir_name.to_ascii_lowercase().as_os_str();
+                    }
+
+                    if search_type == LocateFile && dir_name == query {
                         result.results.push(SearchResult {
-                            filename: vec![
-                                dir_entry.path().display().to_string()
-                            ],
+                            filename: dir_entry.path().display().to_string(),
                             ..SearchResult::default()
                         });
                     }
@@ -35,9 +40,7 @@ pub fn search(dirs: Vec<String>, query: &str, search_type: SearchType, sensitive
                                 let mut content_result = content_search::search(query, &*content, sensitive);
 
                                 if !content_result.content.is_empty() {
-                                    content_result.filename.push(
-                                        dir_entry.path().display().to_string()
-                                    );
+                                    content_result.filename = dir_entry.path().display().to_string();
                                     result.results.push(content_result);
                                 }
                             }
