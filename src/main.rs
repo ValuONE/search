f536a85ce8b0d0a85b9fd022090e589db79d0c2f
ValuONE <vapiendl@gmail.com>
2023-04-07 17:09:10 +0200

    [/] Edited main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 mod model;
 mod lib;
+mod test;
 
 use crate::model::args::SearchArgs;
 use clap::Parser;
+use crate::lib::file_search;
+use crate::lib::utility::get_drive_letter;
 
 fn main() {
-    let test = SearchArgs::parse();
+    let mut config = SearchArgs::parse().convert_to_config();
 
-    println!("{:#?}", test);
+    let mut dirs: Vec<String> = vec![];
+    match config.dir {
+        None => {
+            dirs = get_drive_letter();
+        }
+        Some(dir) => {
+            dirs.push(dir);
+        }
+    }
+
+    let final_result = file_search::search(dirs, &*config.query, config.search_type);
+
+    println!("{:#?}", final_result);
 }
