52f0494784d44b1a0e931858cbe8bc653e452ef6
ValuONE <vapiendl@gmail.com>
2023-04-07 02:11:24 +0200

    [+] Created results.rs

---
+++ src/model/results.rs
@@ -0,0 +1,29 @@
+pub struct FinalResult {
+    pub file_count: i64,
+    pub results: Vec<SearchResult>
+}
+
+impl Default for FinalResult {
+    fn default() -> Self {
+        Self {
+            file_count: 0,
+            results: vec![]
+        }
+    }
+}
+
+pub struct SearchResult {
+    pub filename: Vec<String>,
+    pub content: Vec<String>,
+    pub line: Vec<i64>
+}
+
+impl Default for SearchResult {
+    fn default() -> Self {
+        Self {
+            filename: vec![],
+            content: vec![],
+            line: vec![]
+        }
+    }
+}
