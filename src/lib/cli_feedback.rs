0ba4bfc2d119b45e32fae3976fe3c2b16ee502e5
ValuONE <vapiendl@gmail.com>
2023-04-07 23:26:52 +0200

    [+] Created cli_feedback.rs

---
+++ src/lib/cli_feedback.rs
@@ -0,0 +1,26 @@
+use std::time::Duration;
+use indicatif::{ProgressBar, ProgressStyle};
+use crate::model::results::FinalResult;
+
+pub fn setup_progress_indication(progress_text: String) -> ProgressBar {
+    let pb = ProgressBar::new_spinner();
+
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "▹▹▹▹▹",
+                "▸▹▹▹▹",
+                "▹▸▹▹▹",
+                "▹▹▸▹▹",
+                "▹▹▹▸▹",
+                "▹▹▹▹▸",
+                "▪▪▪▪▪",
+            ]),
+    );
+
+    pb.set_message(progress_text);
+
+    pb
+}
