76d7a3945f5655d0159e1b7827c5c9904596b0a0
ValuONE <vapiendl@gmail.com>
2023-04-07 17:09:06 +0200

    [/] Edited content_search.rs

--- src/lib/content_search.rs
+++ src/lib/content_search.rs
@@ -6,13 +6,15 @@ pub fn search(query: &str, content: &str, sensitive: bool) -> SearchResult {
 
     if !sensitive { query.to_lowercase(); }
 
-    for mut line in content.lines() {
-        line_count += 1;
-        if !sensitive { line.to_lowercase(); }
+    if content.contains(query) {
+        for mut line in content.lines() {
+            line_count += 1;
+            if !sensitive { line.to_lowercase(); }
 
-        if line.contains(query) {
-            search_result.content.push(line.to_string());
-            search_result.line.push(line_count);
+            if line.contains(query) {
+                search_result.content.push(line.to_string());
+                search_result.line.push(line_count);
+            }
         }
     }
 
