081a9766923bbabca910948fd2da1ad19e6d0458
Lukas <bern.9150@gmail.com>
2023-04-07 19:31:33 +0200

    [/] Edited content_search.rs

--- src/lib/content_search.rs
+++ src/lib/content_search.rs
@@ -4,12 +4,14 @@ pub fn search(query: &str, content: &str, sensitive: bool) -> SearchResult {
     let mut search_result =  SearchResult::default();
     let mut line_count: i64 = 0;
 
-    if !sensitive { query.to_lowercase(); }
+    if !sensitive {
+        query.to_lowercase();
+        content.to_lowercase();
+    }
 
     if content.contains(query) {
         for mut line in content.lines() {
             line_count += 1;
-            if !sensitive { line.to_lowercase(); }
 
             if line.contains(query) {
                 search_result.content.push(line.to_string());
